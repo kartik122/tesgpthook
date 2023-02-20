7fff69c615c8c4e4ef32fb07654fc307cb1b6d29
Kartik Gupta <kargupta8@publicisgroupe.net>
2023-02-20 15:04:39 +0530

    testing

--- testJS.js
+++ testJS.js
@@ -1,1 +1,1 @@
-    const num3 = 5 :   const num7 = 3; const n3= 5; const sum = num1 + num2;console.log("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    const num3 = 5 :   val num7 = 3; const n3= 5; const sum = num1 + num2;console.log("The sum of " + num1 + " and " + num2 + " is: " + sum);
