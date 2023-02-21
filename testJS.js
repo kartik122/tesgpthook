11c3b4956deab2533b419074387ee1bf0c485d65
Kartik Gupta <kargupta8@publicisgroupe.net>
2023-02-21 15:34:27 +0530

    testing java and js v3

--- testJS.js
+++ testJS.js
@@ -1,11 +1,4 @@
-const express = require('express');
-const db = require('./db');
-SECRET_PASSWORD = 'gjjhjgka122132jkj';
-router = express.Router();
-const ab = 'sfsa';
-router.get('/email', (req, res) => {
-  db.query('SELECT email FROM users WHERE id = ' + req.query.id).then((rec) => {
-    // logical flow
-    res.send(rec[0]);
-  });
-});
+const num4 = 10;
+num3 = 2;
+const num5 = 0;
+print(num4 + num3);
