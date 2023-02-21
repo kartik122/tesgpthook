18ced35d427fa0d9c7f0835fc2eac7bd5f3a29f4
Kartik Gupta <kargupta8@publicisgroupe.net>
2023-02-21 12:39:48 +0530

    testing java and js v2

--- testJS.js
+++ testJS.js
@@ -2,6 +2,7 @@ const express = require('express');
 const db = require('./db');
 SECRET_PASSWORD = 'abcrdeg';
 router = express.Router();
+const abs = 'sfsa';
 router.get('/email', (req, res) => {
   db.query('SELECT email FROM users WHERE id = ' + req.query.id).then(
     (record) => {
--- testJava.java
+++ testJava.java
@@ -1,6 +1,6 @@
 import org.h2.security.SHA256;
 
-inputString = "s3cr";
+inputString = "s3cr7pk";
 byte[] key = inputString.getBytes();
 
 SHA256.getHMAC(key, message)
