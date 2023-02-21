da3c3ded862b64ff70e728040d88a60ca7d75f38
Kartik Gupta <kargupta8@publicisgroupe.net>
2023-02-21 12:37:02 +0530

    testing js and java files

--- testJS.js
+++ testJS.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const db = require('./db');
-
+SECRET_PASSWORD = 'abcrdeg';
 router = express.Router();
-
 router.get('/email', (req, res) => {
   db.query('SELECT email FROM users WHERE id = ' + req.query.id).then(
     (record) => {
